ff23fb7988761bcda28b0bc631d831c731ddc36a
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-11-22 02:13:19 +0500

    Added buttons to dashboard screen

--- inclusiveBankingApp/app/screens/dashboard.tsx
+++ inclusiveBankingApp/app/screens/dashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import HeaderBarImage from '../components/headerBarImage';
 import theme from '../constants/theme';
+import { Button } from 'react-native-paper'; 
 
 const { height: screenHeight } = Dimensions.get('window');
 
@@ -11,7 +12,7 @@ const DashboardScreen = () => {
     return (
         <View style={styles.container}>
             {/* Header with Image */}
-            <HeaderBarImage imageSource={require('../../assets/images/headerBarLogo.png')} />
+            <HeaderBarImage imageSource={require('../../assets/images/headerBarLogo.png')} personName={personName} />
 
             {/* New Container Below Header */}
             <View style={styles.welcomeContainer}>
@@ -21,7 +22,18 @@ const DashboardScreen = () => {
 
             {/* Main Content */}
             <View style={styles.content}>
-                <Text>Check Balance Screen</Text>
+                <Button mode="contained" style={styles.button} onPress={() => console.log('Transactions')}>
+                    Transactions
+                </Button>
+                <Button mode="contained" style={styles.button} onPress={() => console.log('Bill Payment')}>
+                    Bill Payment
+                </Button>
+                <Button mode="contained" style={styles.button} onPress={() => console.log('History')}>
+                    History
+                </Button>
+                <Button mode="contained" style={styles.button} onPress={() => console.log('Help and Settings')}>
+                    Help & Settings Preferences
+                </Button>
             </View>
         </View>
     );
@@ -53,6 +65,13 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        width: '100%',
+        padding: 10, // padding on left and right
+        paddingVertical: 10,
+    },
+    button: {
+        width: '100%', // make the button span the full width
+        marginVertical: 10, // space between buttons
     },
 });
 
